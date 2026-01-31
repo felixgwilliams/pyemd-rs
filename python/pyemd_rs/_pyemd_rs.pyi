2b451e5ceabdf854ed7a0d625cf2ebd23b8c3fde
Felix Williams <felixgwilliams@users.noreply.github.com>
2026-01-31 10:54:55 +0000

    make imf_check a kwarg

--- python/pyemd_rs/_pyemd_rs.pyi
+++ python/pyemd_rs/_pyemd_rs.pyi
@@ -37,10 +37,10 @@ def normal_mt(seed: int | None, size: int, scale: float) -> npt.NDArray[np.float
 def emd(
     val: npt.NDArray[np.float64],
     max_imf: int | None = None,
-    imf_check: bool = True,
-    *,
-    s_number: int = 0,
     max_iteration: int = 1000,
+    *,
+    imf_check: bool = True,
+    s_number: int = 0,
     svar_thresh: float = 0.001,
     energy_ratio_thresh: float = 0.2,
     std_thresh: float = 0.2,
@@ -53,11 +53,11 @@ def emd(
         val (npt.NDArray[np.float64]): Array to calculate IMFs
         max_imf (int | None, optional): maximum number of IMFs including the residual.
             Defaults to None.
+        max_iteration (int, optional): Maximum number of iterations for EMD. Defaults to 1000
         imf_check (bool, optional): Whether to check the IMF condition and stopping criteria to
             terminate sifting. If False, rely on max_iteration. Defaults to True
         s_number (int, optional): Number of siftings to perform before performing IMF checks
             and stopping criteria. Defaults to 0.
-        max_iteration (int, optional): Maximum number of iterations for EMD. Defaults to 1000
         svar_thresh (float, optional): Variance threshold for EMD. Defaults to 0.001
         energy_ratio_thresh (float, optional): Energy ratio threshold for EMD. Defaults to 0.2
         std_thresh (float, optional): Standard deviation threshold for EMD. Defaults to 0.2
@@ -75,8 +75,8 @@ def ceemdan(
     max_imf: int | None = None,
     seed: int | None = None,
     epsilon: float = 0.005,
-    imf_check: bool = True,
     *,
+    imf_check: bool = True,
     s_number: int = 0,
     parallel: bool = True,
     noise_scale: float = 1.0,
